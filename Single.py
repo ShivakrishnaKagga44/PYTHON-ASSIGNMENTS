08c7ffdf0d93e06203ec706eed5b7674dfdbb4a3
shivakrishnakagga <shivakrishnakagga7@gmail.com>
2026-06-19 18:55:56 +0530

    Add files via upload

---
+++ Single.py
@@ -0,0 +1,29 @@
+
+
+
+n=0
+total=0
+i=1
+while n<=0:
+    n=int(input("enter a number: "))
+    if n<0:
+        print("please provide positive integer numbers")  
+while i<=n:
+    total+=i
+    i+=1
+print(f"Sum of numbers from 1 to {n} is: {total}")
+ 
+
+# n=-9
+# enter a number: -9
+# please provide positive integer numbers
+
+
+# n=-6
+# enter a number: -6
+# please provide positive integer numbers:
+
+
+# n=25
+# enter a number: 25
+# Sum of numbers from 1 to 25 is: 325
